36b20813ffb8f7544c62b608a1d8e9d37da126c4
Roland Boon <roland@rolandboon.com>
2014-09-23 05:04:33 -0700

    add events support

---
+++ angular-clndr.js
@@ -0,0 +1,42 @@
+
+/*!
+ * angular-clndr 0.2.0
+ * 10KB, http://10kb.nl/
+ * License: MIT
+ */
+
+(function() {
+  var module;
+
+  module = angular.module('tien.clndr', []);
+
+  module.directive('tienClndr', function() {
+    return {
+      restrict: 'E',
+      replace: true,
+      transclude: true,
+      scope: {
+        clndr: '=tienClndrObject',
+        events: '=tienClndrEvents'
+      },
+      controller: function($scope, $element, $attrs, $transclude) {
+        return $transclude(function(clone, scope) {
+          var render;
+          $element.append(clone);
+          $scope.$watch('events', function(val) {
+            if (val != null ? val.length : void 0) {
+              return $scope.clndr.setEvents(angular.copy(val));
+            }
+          });
+          render = function(data) {
+            return angular.extend(scope, data);
+          };
+          return $scope.clndr = angular.element("<div/>").clndr({
+            render: render
+          });
+        });
+      }
+    };
+  });
+
+}).call(this);
